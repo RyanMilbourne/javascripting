e89cba2d43cbabcf55bf8d8256c7f5e8d98416dc
Ryan Milbourne <ryanmmilbourne@gmail.com>
2023-08-23 21:12:59 -0700

    Adding my own js file to confirm whether or not I've gone insane.

---
+++ sanity-test.js
@@ -0,0 +1,10 @@
+let encouragingMessage = "You've got this!";
+let discouragingMessage = "You aint got this";
+
+let i = 4;
+
+if(i >= 4){
+  console.log(encouragingMessage);
+} else {
+  console.log(discouragingMessage);
+}
